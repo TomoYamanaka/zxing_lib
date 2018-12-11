546cc2e665b9f9c438d1b976b221b59312a1907e
TomoYamanaka <tomoyuki.yamanaka.xd@renesas.com>
2018-12-11 11:32:47 +0900

    First commit and support ARMCC toolchain.

---
+++ zxing_config.h
@@ -0,0 +1,21 @@
+
+/**** User Selection *********/
+
+//#define ZXING_EXCLUDE_1DCODE                      /* define : 1D barcodes can not be scanned */
+#undef ZXING_EXCLUDE_1DCODE                         /* undef  : 1D barcodes can be scanned */
+                                                    
+//#define ZXING_EXCLUDE_QRCODE                      /* define : QR-code can not be scanned */
+#undef ZXING_EXCLUDE_QRCODE                         /* undef  : QR-code can be scanned */
+                                                    
+//#define ZXING_EXCLUDE_MATRIX                      /* define : Matrix can not be scanned */
+#undef ZXING_EXCLUDE_MATRIX                         /* undef  : Matrix can be scanned */
+                                                    
+//#define ZXING_EXCLUDE_AZTEC                       /* define : Aztec can not be scanned */
+#undef ZXING_EXCLUDE_AZTEC                          /* undef  : Aztec can be scanned */
+                                                    
+//#define ZXING_EXCLUDE_PDF417                      /* define : PDF417 can not be scanned */
+#undef ZXING_EXCLUDE_PDF417                         /* undef  : PDF417 can be scanned */
+
+//#define ZXING_MULTI_QUADRANT                      /* define : Zxing can multi decode */
+#undef ZXING_MULTI_QUADRANT                         /* undef  : Zxing can not multi decode */
+
